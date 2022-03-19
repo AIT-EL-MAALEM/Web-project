1a457c0de78a96a2b8c3820207f6997bc1bd0b34
AIT-EL-MAALEM <76658991+AIT-EL-MAALEM@users.noreply.github.com>
2022-03-19 19:23:27 +0100

    Add files via upload

---
+++ remplissage.js
@@ -0,0 +1,4 @@
+function ajouter(){
+	document.getElementById('tab').innerHTML+="<tr><td>"+document.getElementById('nom').value+
+	"</td><td>"+document.getElementById('prenom').value+"</td></tr>";
+}
